9e2911238c70d21516d1d7257515e6a34e460825
JHH <wjdgo813@naver.com>
2018-09-28 22:21:53 +0900

    init

---
+++ SiriShortcutPrac/SiriShortcutPrac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriShortcutPrac
+//
+//  Created by Haehyeon Jeong on 2018. 9. 28..
+//  Copyright © 2018년 JHH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
