3844636fd5f1ea54766e221ac5eb5631a6cc50b4
wjdgo813 <wjdgo813@naver.com>
2018-10-08 19:52:35 +0900

    simple short cut example

--- SiriShortcutPrac/AppDelegate.swift
+++ SiriShortcutPrac/AppDelegate.swift
@@ -41,6 +41,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        let viewController = window?.rootViewController as! ViewController
+        viewController.makeViewRed()
+        
+        return true
+    }
 
 }
 
--- SiriShortcutPrac/ViewController.swift
+++ SiriShortcutPrac/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import Intents
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        let activity = NSUserActivity(activityType: "JHH.SiriShortcutPrac.makeRed")
+        activity.title = "빨강으로 만들자."
+        activity.userInfo = ["color":"red"]
+        activity.isEligibleForSearch = true
+        activity.isEligibleForPrediction = true
+        activity.persistentIdentifier = NSUserActivityPersistentIdentifier(stringLiteral: "JHH.SiriShortcutPrac.makeRed")
+        view.userActivity = activity
+        activity.becomeCurrent()
+        
+        makeViewRed()
+    }
+    
+    func makeViewRed(){
+        view.backgroundColor = .red
+    }
 }
 
